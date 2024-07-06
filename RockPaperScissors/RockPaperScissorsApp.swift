ce3e4d12a755f73c5171d125ce2cf40badbfc2cb
yanagyi <yanagyi429@gmail.com>
2024-07-06 16:23:27 +0900

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Hiro Yanagisawa on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
