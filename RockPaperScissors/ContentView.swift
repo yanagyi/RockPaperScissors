c28f31fca4990e926cf5848ce59db613cbcad9f5
yanagyi <yanagyi429@gmail.com>
2024-07-07 02:01:57 +0900

    グーチョキパーを乱数で表示させる実装

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,18 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var outputText = "Hello World!"
+    //画面表示の変数
+    @State var jankenNumber = 0
     var body: some View {
         VStack {
-            Text(outputText)
-                .font(.largeTitle)
-            Button("切り替えボタン") {
-                outputText = "Hi! Swift!"
+            if jankenNumber == 0 {
+                Text("これからじゃんけんをします！")
             }
-            .padding(.all)
-            .background(.blue)
-            .foregroundColor(.white)
+            else if jankenNumber == 1 {
+                //グーの画像を表示
+                Image(.gu)
+                //リサイズを指定
+                    .resizable()
+                //画面に収まるようにアスペクト比を維持する設定
+                    .scaledToFit()
+                //じゃんけんの種類を指定
+                Text("グー")
+            }
+            else if jankenNumber == 2 {
+                //チョキの画像を表示
+                Image(.choki)
+                //リサイズを指定
+                    .resizable()
+                //画面に収まるようにアスペクト比を維持する設定
+                    .scaledToFit()
+                //じゃんけんの種類を指定
+                Text("チョキ")
+            }
+            else {
+                //パーの画像を表示
+                Image(.pa)
+                //リサイズを指定
+                    .resizable()
+                //画面に収まるようにアスペクト比を維持する設定
+                    .scaledToFit()
+                //じゃんけんの種類を指定
+                Text("パー")
+            }
+            Button(action: {
+                print("タップされたよ")
+                jankenNumber = Int.random(in: 1...3)
+            }, label: {
+                Text("じゃんけんをする")
+            })
         }
+        
     }
 }
 
