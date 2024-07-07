adc095f574e2baaf911ad2501da922f8b5ede157
yanagyi <yanagyi429@gmail.com>
2024-07-07 11:39:31 +0900

    じゃんけんゲームα版実装

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -12,8 +12,13 @@ struct ContentView: View {
     @State var jankenNumber = 0
     var body: some View {
         VStack {
+            //画面上部にスペースを設ける
+            Spacer()
+            
             if jankenNumber == 0 {
                 Text("これからじゃんけんをします！")
+                //下辺に余白を設定
+                    .padding(.bottom)
             }
             else if jankenNumber == 1 {
                 //グーの画像を表示
@@ -22,8 +27,12 @@ struct ContentView: View {
                     .resizable()
                 //画面に収まるようにアスペクト比を維持する設定
                     .scaledToFit()
+                //画面にスペースを設ける
+                Spacer()
                 //じゃんけんの種類を指定
                 Text("グー")
+                //下辺に余白を設定
+                    .padding(.bottom)
             }
             else if jankenNumber == 2 {
                 //チョキの画像を表示
@@ -32,8 +41,12 @@ struct ContentView: View {
                     .resizable()
                 //画面に収まるようにアスペクト比を維持する設定
                     .scaledToFit()
+                //画面上部にスペースを設ける
+                Spacer()
                 //じゃんけんの種類を指定
                 Text("チョキ")
+                //下辺に余白を設定
+                    .padding(.bottom)
             }
             else {
                 //パーの画像を表示
@@ -42,8 +55,12 @@ struct ContentView: View {
                     .resizable()
                 //画面に収まるようにアスペクト比を維持する設定
                     .scaledToFit()
+                //画面上部にスペースを設ける
+                Spacer()
                 //じゃんけんの種類を指定
                 Text("パー")
+                //下辺に余白を設定
+                    .padding(.bottom)
             }
             Button(action: {
                 print("タップされたよ")
@@ -63,6 +80,12 @@ struct ContentView: View {
                 jankenNumber = newJankenNmber
             }, label: {
                 Text("じゃんけんをする")
+                //ボタンの装飾
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .background(.pink)
+                    .foregroundColor(.white)
             })
         }
         
